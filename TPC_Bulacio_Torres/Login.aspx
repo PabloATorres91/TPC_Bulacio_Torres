bb5558d8409e9e3df66455aed21372a9d0d0a728
Pablo Torres <p.torres0991@gmail.com>
2021-06-21 02:50:55 -0300

    Agregando primera versión de formulario login.

---
+++ TPC_Bulacio_Torres/Login.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="TPC_Bulacio_Torres.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container well contenedor">
+            <div style="text-align:center;">
+                <h2>Iniciar sesión</h2>
+            </div>
+        <form runat="server" class="form-horizontal">
+            <%-- Agregamos controles de Usuario --%>
+            <div class="form-group">
+                <asp:Label ID="lblUser" runat="server" Text="Usuario" CssClass="control-label col-sm-2"></asp:Label>
+                <div class="col-sm-10">
+                    <asp:TextBox ID="txtUser" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+            <%-- Agregamos controles de Password--%>
+            <div class="form-group">
+                <asp:Label ID="lblPassword" runat="server" Text="Contraseña" CssClass="control-label col-sm-2"></asp:Label>
+                <div class="col-sm-10">
+                    <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+            <%-- Agregamos control de boton--%>
+            <div class="form-group">
+                <asp:Button ID="btnAccept" runat="server" Text="Aceptar" OnClick="btnAccept_Click" CssClass="form-control btn btn-primary"/>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ TPC_Bulacio_Torres/Login.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_Bulacio_Torres
+{
+
+
+    public partial class Login
+    {
+
+        /// <summary>
+        /// Control lblUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUser;
+
+        /// <summary>
+        /// Control txtUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUser;
+
+        /// <summary>
+        /// Control lblPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPassword;
+
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+
+        /// <summary>
+        /// Control btnAccept.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAccept;
+    }
+}
